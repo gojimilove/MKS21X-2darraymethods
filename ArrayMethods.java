ff83f42202ea11467d44a4c7182339e0445a968b
Jessica Kim <jkim00@stuy.edu>
2018-11-06 17:50:46 -0500

    working columnSum(int[][] ary, int x)

--- ArrayMethods.java
+++ ArrayMethods.java
@@ -8,11 +8,16 @@ public class ArrayMethods {
 		}
 		return result;
 	}
-/*
+
 	public static int columnSum(int[][] ary, int x) {
-
+		int result = 0;
+		for (int i = 0; i < ary.length; i++) {
+			if (x >= ary[i].length) result+= ary[i][0];
+			else result+= ary[i][x];
+		}
+		return result;
 	}
-
+/*
 	public static int[] allRowSums(int[][] ary) {
 
 	}
