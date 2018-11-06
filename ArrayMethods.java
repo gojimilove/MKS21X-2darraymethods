5bdd7db3cc4bb4a475c93976a97bfc944d8aabf0
Jessica Kim <jkim00@stuy.edu>
2018-11-06 16:50:52 -0500

    file ArrayMethods.java added

---
+++ ArrayMethods.java
@@ -0,0 +1,25 @@
+public class ArrayMethods {
+	public static int rowSum(int[][] ary, int x) {
+
+	}
+
+	public static int columnSum(int[][] ary, int x) {
+
+	}
+
+	public static int[] allRowSums(int[][] ary) {
+
+	}
+
+	public static int[] allSolSums(int[][] ary) {
+
+	}
+
+	public static boolean isRowMagic(int[][] ary) {
+
+	}
+
+	public boolean isColumnMagic(int[][] ary) {
+		
+	}
+}
